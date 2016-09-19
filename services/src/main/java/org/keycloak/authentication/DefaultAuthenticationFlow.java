ecb24e90dff56a745f7ef885fa077f7461c16ec8
Thomas Darimont <thomas.darimont@gmail.com>
2016-09-20 00:49:42 +0200

    KEYCLOAK-3314 PoC for amr support in IDToken
    (cherry picked from commit e95edda495cd34b4e745cb98b43505c3fa752f20)
    (cherry picked from commit 38ab1bf673bcfeefb565610da13ccda1969336c9)

--- services/src/main/java/org/keycloak/authentication/DefaultAuthenticationFlow.java
+++ services/src/main/java/org/keycloak/authentication/DefaultAuthenticationFlow.java
@@ -23,6 +23,7 @@ import org.keycloak.models.AuthenticationFlowModel;
 import org.keycloak.models.ClientSessionModel;
 import org.keycloak.models.UserModel;
 import org.keycloak.services.ServicesLogger;
+import org.keycloak.services.managers.AuthenticationManager;
 
 import javax.ws.rs.core.Response;
 import java.util.Iterator;
@@ -203,6 +204,13 @@ public class DefaultAuthenticationFlow implements AuthenticationFlow {
             case SUCCESS:
                 logger.debugv("authenticator SUCCESS: {0}", execution.getAuthenticator());
                 processor.getClientSession().setExecutionStatus(execution.getId(), ClientSessionModel.ExecutionStatus.SUCCESS);
+
+                String authMethods = processor.getClientSession().getNote(AuthenticationManager.AUTH_METHODS);
+                if(authMethods == null){
+                    authMethods = "";
+                }
+                processor.getClientSession().setNote(AuthenticationManager.AUTH_METHODS, authMethods + " " + execution.getAuthenticator());
+
                 if (execution.isAlternative()) alternativeSuccessful = true;
                 return null;
             case FAILED:
